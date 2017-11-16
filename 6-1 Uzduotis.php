a45f6f66e49fc01c9c82dfc27ac7db8e5f5cdd81
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-16 11:41:23 +0200

    Veikia

--- 6-1 Uzduotis.php
+++ 6-1 Uzduotis.php
@@ -1,3 +1,12 @@
 <?php
-$a = [5, 6, 10, 15]
-$b = [8, 5, 3, 25]
+$a = array (5, 6, 10, 15);
+$b = array (8, 5, 3, 25);
+function vidurkisA($a) {
+    return array_sum($a) / count($a);
+   }
+function vidurkisB($b) {
+    return array_sum($b) / count($b);
+   }
+   $x = vidurkisA($a) - vidurkisB($b);
+   echo $x;
+?>

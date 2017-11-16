e598306312b52e3d59edec7add7c0e2ca9d920e9
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-16 14:34:03 +0200

    Update Masyvas.php

--- Masyvas.php
+++ Masyvas.php
@@ -1,9 +1,16 @@
 <?php
-$a = array(4, 3, 1, 2);
-ksort($a);
- 
-$numlength = count($a);
-for($i = 0; $i < $numlength; $i++) {
-    echo $a[$i]; echo "<br>";
-}
+	 $a=array(4,3,1,2);
+ for ($i=0; $i<count($a); $i++){
+    for ($j=0; $j<count($a); $j++) {
+      if ($a[$j] > $a[$i]){
+        $x = $a[$i];
+        $a[$i] = $a[$j];
+        $a[$j] = $x;
+      }
+    }
+ }
+
+ for($i=0;$i<count($a);$i++){
+   echo $a[$i].",";
+ }
 ?>

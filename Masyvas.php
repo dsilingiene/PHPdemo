de69e5abe6cd41b51be2a48bad4cbc833dadc7fa
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-17 14:20:12 +0200

    Update Masyvas.php

--- Masyvas.php
+++ Masyvas.php
@@ -2,7 +2,7 @@
  $a=array(4,3,1,2);
  for ($i=0; $i<count($a); $i++){
     for ($j=0; $j<count($a); $j++) {
-      if ($a[$j] > $a[$i]){
+      if ($a[$j] < $a[$i]){
         $x = $a[$i];
         $a[$i] = $a[$j];
         $a[$j] = $x;

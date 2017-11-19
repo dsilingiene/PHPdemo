ed1fb2002dabb65f576c0d59e32f1b62b42eba1b
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-19 14:25:48 +0200

    Jonas Petras skirtingos poros

---
+++ JonasPetras.php
@@ -0,0 +1,15 @@
+<?php
+/*#7 - 1/2 Uždaviniai - Turime masyvą $a = [‘Jonas’, ‘Petras’, ‘Antanas’, ‘Povilas’]. 
+1. Atspausdinkite visas galimas skirtingas poras laikant, kad pvz poros 
+‘Jonas’ - ‘Petras’ ir ‘Petras’ - ‘Jonas’ yra tokios pat. 
+2. Ta pati sąlyga tik pvz poros ‘Jonas’ - ‘Petras’ ir ‘Petras’ - ‘Jonas’ yra laikomos skirtingomis.*/
+$a = array(‘Jonas’, ‘Petras’, ‘Antanas’, ‘Povilas’);
+$poros = '';
+for ($i=0; $i<count($a); $i++){
+    for ($j=0; $j<4; $j++){
+            $poros .= $a[$i] . $a[$j] . "<br />";  
+        }
+    
+}
+echo "<h1>Poros:</h1>$poros";
+?>

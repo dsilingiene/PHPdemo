06dc036595b73617616479793c11f9f67eb961a9
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-20 13:15:12 +0200

    7/3 užduotis

---
+++ MasyvosStulpeliuSuma.php
@@ -0,0 +1,13 @@
+<?php
+/*#7 - 3 Uždavinys - Turime daugiamatį masyvą, kuris aprašo kažką panašaus
+ į Excel lentelę arba matricą, t.y. pirmas indeksas žymi eilutę, o antras stulpelį. 
+ Eilutės gali turėti skirtingą elementų (stulpelių) skaičių. 
+ Suskaičiuokite stulpeliuose esančių skaičių sumas ir išveskite didžiausią
+testuokite su masyvu:  $a = [ [1, 3, 4], [2, 5], [2 => 3, 5=> 8], [1, 1, 5 => 1] ];
+*/
+
+$a =  [ [1, 3, 4], [2, 5], [2 => 3, 5=> 8], [1, 1, 5 => 1] ];
+
+
+
+?>

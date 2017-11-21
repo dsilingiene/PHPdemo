5cfbedd1048f3284db38678b95ce686edad0e4b6
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-21 22:12:41 +0200

    9/2 uzduotis  3 žmonės lentelėje

---
+++ OOP.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+<?php
+//#9 - Pratimas 2 - Sukurkite masyvą,
+// kuriame būtų bent trys Zmogus klasės objektai ir atspausdinkite visus žmones lentelėje.
+
+class Zmogus
+{
+public $vardas;
+public $pavarde;
+
+    function __construct($v, $p) {
+         $this ->vardas = $v;
+         $this ->pavarde = $p;
+    }
+}
+
+$zmones = [
+    new Zmogus ('Jonas', 'Jonaitis'),
+    new Zmogus ('Ieva', 'Ievaite'),
+    new Zmogus ('Petras','Petraitis')
+];
+?>
+    <table>
+        <tr>
+            <th>Vardas</th><th>Pavarde</>
+        </tr>
+        <?php foreach ($zmones as $zmogus): ?>
+            <tr>
+                <td><?php echo $zmogus->vardas; ?></td>
+                <td><?php echo $zmogus->pavarde; ?></td>
+            </tr>
+        <?php endforeach;?>
+    </table>
+</body>
+</html>
+
--- VyraiMoterys.php
+++ VyraiMoterys.php
@@ -1,7 +1,7 @@
 <?php
 //Atspausdinkite visas galimas skirtingas poras vyras - moteris.
 
-$zmones = [ 
+$a = [ 
 [’vardas’ => ‘Jonas’, ‘lytis’ => ‘V’], 
 [‘vardas’ => ‘Ona’, ‘lytis’ => ‘M’], 
 [‘vardas’ => ‘Petras’, ‘lytis’ => ‘V’], 
@@ -9,10 +9,23 @@ $zmones = [
 [‘vardas’ => ‘Eglė’, ‘lytis’ => ‘M’]
 ]; 
 
+else echo nėra duomenų
 var_dump ($zmones);
 
 
+//tas pats kaip 7.1 jonas jonas, tik turim ne vardus, o atskirus masyvus.
+//du ciklai, plius dar 1 tikrinimas. foreach if foreach if vyrai moterys
+//6 poros
 
+//8.2 for ciklas kaip 1 uzd
+// pirma zmogu grupuojam su tolimesniais, nes artimi sugrupuoti.
+//lyginti indeksus, ne vardus
+//tikrinti su vienodu vardu - turi indeksai būt skirtingi
+//if, for, for, for, if echo - 2 būdas + tikrinimas ar komandoj visi zmones yra tos pacios lyties - tik atvirkscia sąlyga.tikrinam blogą sąlygą, tada suskliaudziam paneigiam, 
+//dedam sauktuką sakom ne. salygą verčiam atvirkšciai naikinam sklaistus. 
 
+// jeigu verčiant atvirkščiai sąlygos sujungtos ir arba , įsivaizduojam kad yra skliaustai.
 
+//git extensionas susiinstaliuoti
 ?>
+

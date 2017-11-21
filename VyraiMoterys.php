9ab1a327076322f251bf68bd0476169c82888066
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-21 13:02:01 +0200

    8/1 užduotis

---
+++ VyraiMoterys.php
@@ -0,0 +1,18 @@
+<?php
+//Atspausdinkite visas galimas skirtingas poras vyras - moteris.
+
+$zmones = [ 
+[’vardas’ => ‘Jonas’, ‘lytis’ => ‘V’], 
+[‘vardas’ => ‘Ona’, ‘lytis’ => ‘M’], 
+[‘vardas’ => ‘Petras’, ‘lytis’ => ‘V’], 
+[‘vardas’ => ‘Marytė’, ‘lytis’ => ‘M’], 
+[‘vardas’ => ‘Eglė’, ‘lytis’ => ‘M’]
+]; 
+
+var_dump ($zmones);
+
+
+
+
+
+?>

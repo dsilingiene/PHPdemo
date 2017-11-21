c9c41664bebb594f0c1de86e1794a2c51758bf80
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-21 06:19:44 +0200

    7/1

---
+++ SkirtingosPoros.php
@@ -0,0 +1,9 @@
+<?php
+$a = array(‘Jonas’, ‘Petras’, ‘Antanas’, ‘Povilas’);
+for ($i=0; $i<count($a) - 1; $i++){
+    for ($j=$i + 1; $j<count($a); $j++){
+            echo $a[$i]. "-". $a[$j] . "<br />";  
+        }
+    
+}
+?>

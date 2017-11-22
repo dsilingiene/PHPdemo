b4a74b0c5ca3285a248f6aea65e9fcd06fe60a03
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-22 15:14:45 +0200

    nesigauna

---
+++ 9.1MokiniaiLenteleje.php
@@ -0,0 +1,17 @@
+<?php
+
+$mokiniai = [
+['vardas' => 'Jonas', 'pazymiai' => ['lietuviu' => [4, 8, 6, 7], 'anglu' =>[6, 7, 8], 'matematika' => [3, 5, 4]]], 
+['vardas' => 'Ona', 'pazymiai' => ['lietuviu' => [10, 9, 10], 'anglu' => [9, 8, 10], 'matematika' => [10, 10, 9, 9]]]
+];
+
+/*Išveskite mokinių sąrašą į html lentelę taip (pagal stulpelius): 
+1) mokinio vardas 
+2) visi lietuvių kalbos pažymiai 
+3) lietuvių kalbos vidurkis 
+4) visi anglų kalbos pažymiai
+5) anglų kalbos vidurkis 
+6) visi matematikos pažymiai
+7) matematikos vidurkis 
+8) bendras vidurkis*/
+

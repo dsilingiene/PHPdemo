e6ba3638d58964c753783d1cd2bb6fc2b36e8615
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-29 13:21:14 +0200

    12 Užduotis

--- 8.3.php
+++ 8.3.php
@@ -8,10 +8,44 @@ $mokiniai = [
 Suskaičiuokite kuris geriausiai mokosi pagal visų dalykų pažymių vidurkius. 
 Dalyko pažymio nustatymui reikės panaudoti funkciją round()*/
 
-//echo '<pre>', print_r($mokiniai);'</pre>';
+foreach($mokiniai as $k => $mokinys) {
+    foreach ($mokinys['pazymiai'] as $dal => $pazymys {
+          for ($i=0; $i<count($pazymys); $i++){
+            
+        }
+    }
+}
+echo "<b>Geriausiai mokosi: </b><br>";
+$ger = 0;
+$vidSuma = vidurkiuSuma($mokiniai);
+$n = count($vidSuma);
+for ($i=0; $i<$n; $i++){
+    if ($ger < $vidSuma[$i]['vidurkis']){
+        $ger = $vidSuma[$i]['vidurkis']; 
+        $vardas = $vidSuma[$i]['vardas'];
+}
+}
+echo "<br><b>Geriausia vidurkių suma: </b>" .$vardas ." - ".$ger;
+  
 
 
-
-
-
+function vidurkiuSuma($mokiniai){
+foreach ($mokiniai as $key => $d){
+    $vidSuma = 0;
+    foreach ($d['pazymiai'] as $k=> $value){
+        $vidSuma += vidurkis ($value);
+    }
+    $maxVidurkis[] = ['vardas'=>$d['vardas'], 'vidurkis'=>$vidSuma];
+    echo $d['vardas'] ." - Vidurkių suma yra: " .$vidSuma ."<br>";
+}
+return $maxVidurkis;
+}
+function vidurkis($dalykas){
+$sum = 0;
+foreach ($dalykas as $ivertinimas){
+    $sum += $ivertinimas;
+}
+$vid = round($sum/count($dalykas));
+return $vid;
+}
 ?>
---
+++ automobiliaiperraso.php
@@ -0,0 +1,90 @@
+<?php 
+session_start();
+$_SESSION["automobiliai"]=$automobiliai;
+echo $_SESSION["automobiliai"];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Automobiliai</title>
+</head>
+<body>
+<h1>12 Užduotis</h1>
+<?php
+
+/*#12 - Uždavinys - Padarykite formą įvesti automobilius
+
+1) Greičio fiksavimo data ir laikas, pvz: 2016-12-31 23:15:25
+2) Automobilio numeris, pvz: ABC 001 
+3) Nuvažiuotas atstumas metrais
+4) Sugaištas laikas sekundėmis
+Tegul programa atsimena (session arba cookie) visus suvestus automobilius ir, 
+žemiau įvedimo formos, išveda juos greičių mažėjimo tvarka į html lentelę.*/
+?>
+
+<h1>Automobiliai</h1>
+
+    <form method="post" action="automobiliai.php">
+    <br><label>Greičio fiksavimo data ir laikas: </label><input type="text" name="date" required/></br>
+    <br><label>Automobilio numeris, pvz: ABC 001: </label><input type="text" name="number" required/></br>
+    <br><label>Nuvažiuotas atstumas metrais: </label><input type="number" name="distance" required/></br>
+    <br><label>Sugaištas laikas sekundėmis: </label><input type="number" name="time" required/></br>
+    <br> <button type="submit">Įvesti</button></br>
+    <br></form>
+
+ <?php
+
+class Automobiliai {
+    public $date;
+    public $number;
+    public $distance;
+    public $time;
+
+    function __construct($dat, $num, $dist, $t) {
+        $this->date = $dat;
+        $this->number = $num;
+        $this->distance = $dist;
+        $this->time = $t;
+    }
+
+    function greitisKmh() {
+        $greitisMs = $this->distance/$this->time;
+        $greitisKmh = $greitisMs*3.6;
+        return round($greitisKmh,1);
+    }
+}
+
+    if (isset($_POST['date'])) {
+        $naujas = new Automobiliai($_POST['date'], $_POST['number'], $_POST['distance'], $_POST['time']);
+        $automobiliai[] = $naujas;
+        $_SESSION['automobiliai'] = $automobiliai;
+    }
+
+?>
+
+
+ <table border="1">
+ <caption><h1>Automobiliai</h1></caption>
+ <tr>
+     <th>Automobilio numeris</th>
+     <th>Data ir laikas</th>
+     <th>Nuvažiuotas atstumas</th>
+     <th>Sugaištas laikas</th>
+     <th>Greitis Km/val</th>
+ <tr>
+ <?php foreach ($automobiliai as $automobiliai): ?>
+ <tr>
+     <td><?php echo $automobiliai->date; ?></td>
+     <td><?php echo $automobiliai->number; ?></td>
+     <td><?php echo $automobiliai->distance; ?></td>
+     <td><?php echo $automobiliai->time; ?></td>
+     <!--<td><?php echo greitisKmh(); ?></td>-->
+     
+ </tr>
+<?php endforeach; ?>
+ </tr>
+    
+</table>
+</body>
+</html>
+

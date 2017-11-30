4e62f0b363dfcb3963696c08cea292afb5fc7e8d
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-30 16:42:22 +0200

    Galutinis veikiantis

---
+++ 13 Užduotis Reguliarios išraiškos.php
@@ -0,0 +1,11 @@
+<?php
+/*Duota /demo/19/automobilis?id=140
+
+Pasitikrinimui pasinaudokite https://regex101.com/
+1) rasti tekstą nuo pradžios iki ? (/demo/19/automobilis)
+2) raskite tekstą nuo ? iki galo (id=140)
+3) raskite tekstą iki antro / (/demo)
+4) raskite tekstą tarp paskutinio / ir ? (automobilis)*/
+
+?>
+

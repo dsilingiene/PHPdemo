91b60005097cffd689d2b256b6443d8b35d7bd9d
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-11-30 17:07:42 +0200

    Galutinis veikiantis

--- 13 Užduotis Reguliarios išraiškos.php
+++ 13 Užduotis Reguliarios išraiškos.php
@@ -2,8 +2,8 @@
 /*Duota /demo/19/automobilis?id=140
 
 Pasitikrinimui pasinaudokite https://regex101.com/
-1) rasti tekstą nuo pradžios iki ? (/demo/19/automobilis)
-2) raskite tekstą nuo ? iki galo (id=140)
+1) rasti tekstą nuo pradžios iki ? (/demo/19/automobilis)   
+2) raskite tekstą nuo ? iki galo (id=140):  [^\?]+$
 3) raskite tekstą iki antro / (/demo)
 4) raskite tekstą tarp paskutinio / ir ? (automobilis)*/
 

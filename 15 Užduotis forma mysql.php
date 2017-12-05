189f71e57d94d8d6cf6ef909a832eb40ff76c481
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-12-05 23:21:53 +0200

    nelabai

--- 15 Užduotis forma mysql.php
+++ 15 Užduotis forma mysql.php
@@ -7,8 +7,10 @@
 <body>
 <h1>15 Užduotis</h1>
 <?php
+
 /*Sukurkite programą, kad galima būtų įvesti naujus radarų įrašus ir taisyti 
 jau suvestą informaciją. Taip pat kad galima būtų peržiūrėti jau turimus įrašus.*/
+
 $servername = "localhost";
 $username = "Auto";
 $password = "LabaiSlaptas123";
@@ -20,79 +22,92 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Nepavyko prisijungti: " . $conn->connect_error);
 } 
-$sql="INSERT INTO `radars` (id, date, number, distance, time)
-VALUES 
-('{$_POST['id']}','{$_POST['date']}','{$_POST['number']}','{$_POST['distance']}','{$_POST['time']}')";
 
-$page = 1;
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-}
-if ($page != 2) $page = 100;
-if (isset($_GET['offset'])) {
-    $offset = $_GET['offset'];
-} else {
-    $offset = 0;
-}
+$values = [];
+?>
 
-$sql = 'SELECT `id`,`date`,`number`, `distance`,`time`,`distance`/`time`*3.6 as `speed` FROM radars ORDER BY `id`, `date` DESC LIMIT ' . ($page + 1) . ' OFFSET ' . $offset;
+<?php 
+
+    if (array_key_exists('id', $_GET) && $_GET['id'] > 0) {
+        $sql = 'SELECT * FROM radars WHERE `id` = ' . $_GET["id"];
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $values = $result->fetch_assoc();
+        }
+    }
+
+    if ($_POST['id'] > 0) {
+        $sql = "UPDATE radars SET `number` = ?, `date` = ?, `distance` = ?, `time` = ? WHERE id = ?"; 
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bind_param("ssddi", $_POST['number'], $_POST['date'], 
+            $_POST['distance'], $_POST['time'], $_POST['id']);
+        $stmt->execute();
+
+        header("Location: " . $_SERVER['PHP_SELF']); /* Redirect browser */
+        exit();
+
+    } else if ($_POST['id'] === '') {
+        $insert = "INSERT INTO radars(`date`, `number`, `distance`, `time`) VALUES(?, ?, ?, ?)"; 
+        $stmt = $conn->prepare($insert);
+        $stmt->bind_param("ssdd", $_POST['date'], $_POST['number'], $_POST['distance'], $_POST['time']);
+        $stmt->execute();
+
+        header("Location: " . $_SERVER['PHP_SELF']); /* Redirect browser */
+        exit();
+    }
+
+
+?>
+<form method="post" action=""><br>
+<br><label>Greičio fiksavimo data ir laikas: </label><input name="date" type="text" value = "<?=$values['date'] ?>" </br></br>
+<br><label>Automobilio numeris, pvz: ABC 001: </label><input type="text" value = "<?=$values['number'] ?>"</br></br>
+<br><label>Nuvažiuotas atstumas metrais: </label><input type="text" value = "<?=$values['distance'] ?>"</br/></br>
+<br><label>Sugaištas laikas sekundėmis: </label><input type="text" value = "<?=$values['time'] ?>"</br/></br>
+<input name="id" type="hidden" value="<?= $values['id'] ?>">
+<br> <button type="submit">Įvesti</button></br>
+<br></form>
+<form action="" method="get"><button>Valyti</button></form></br>
+
+<?php
+lentele($conn);
+
+
+function lentele($conn) {
+// išvedame
+$sql = 'SELECT *, `id`, `date`, `number`, `distance`, `time` FROM radars ORDER BY `id`, `date` DESC';
 $result = $conn->query($sql);
-if ($result->num_rows > 0) { 
+if ($result->num_rows > 0) {
     ?>
-<form method="post" action="">
-    <br><label>Greičio fiksavimo data ir laikas: </label><input type="text" name="date" required/></br>
-    <br><label>Automobilio numeris, pvz: ABC 001: </label><input type="text" name="number" required/></br>
-    <br><label>Nuvažiuotas atstumas metrais: </label><input type="number" name="distance" required/></br>
-    <br><label>Sugaištas laikas sekundėmis: </label><input type="number" id="time" name="time" value="<?= $time ?>" require/></br>
-    <br> <button type="submit">Įvesti</button></br>
-    <br></form>
-
-    <?php if ($offset > 0): ?>
-    <!-- <form>
-        <input type="hidden" name="offset" value="<?= $offset >= $page ? $offset - $page : 0 ?>">
-        <button>Atgal</button>    
-    </form> -->
-        <a href="<?= "?offset=".($offset >= $page ? $offset - $page : 0) ?>">Atgal</a>
-    <?php endif; ?>
-
-    <?php if ($result->num_rows == $page + 1): ?>
-    <!-- <form>
-        <input type="hidden" name="offset" value="<?= $offset + $page ?>">
-        <button>Pirmyn</button>    
-    </form> -->
-        <a href="<?= "?offset=".($offset + $page) ?>">Pirmyn</a>
-    <?php endif; ?>
-
-    <table border=1>
+    <table border=1px>
         <tr>
-            <th>Id</th>
+            <th>ID</th>
             <th>Data ir laikas</th>
-            <th>Automobilio numeris</th>
-            <th>Atstumas, m</th>
+            <th>Numeris</th>
+            <th>Atstumas (m)</th>
             <th>Laikas, s</th>
-            <th>Greitis, Km/h</th>
+            <th>Duomenų taisymas</th>
+            
         </tr>
     
+        <?php while($row = $result->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['date']; ?></td>
+                <td><?php echo $row['number']; ?></td>
+                <td><?php echo $row['distance']; ?></td>
+                <td><?php echo $row['time']; ?></td>
+                <td><a href="?id=<?= $row['id'] ?>">Taisyti</a></td>
+            </tr>
+        <?php endwhile; ?>
+    
+    </table>
+
     <?php
-    // output data of each row
-    //while($row = $result->fetch_assoc()) {
-    for ($i = 0; $i < $page; $i++) {
-        if (!($row = $result->fetch_assoc())) break;
-        ?>
-        <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['date']; ?></td>
-            <td><?php echo ($row['number']); ?></td>
-            <td><?php echo ($row['distance']); ?></td>
-            <td><?php echo ($row['time']); ?></td>
-            <td><?php echo round($row['speed']); ?></td>
-        </tr>
-        <?php
-    }
-    echo '</table>';
 } else {
-    echo 'Nėra duomenų';
+    echo 'nėra duomenų';
 }
 $conn->close();
+}
 ?>
 </body>

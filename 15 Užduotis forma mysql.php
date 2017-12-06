11d6d2aac506830844c03a67704b23ecec1fde37
dsilingiene <33618489+dsilingiene@users.noreply.github.com>
2017-12-06 10:42:41 +0200

    Veikia

--- 15 Užduotis forma mysql.php
+++ 15 Užduotis forma mysql.php
@@ -29,7 +29,7 @@ $values = [];
 <?php 
 
     if (array_key_exists('id', $_GET) && $_GET['id'] > 0) {
-        $sql = 'SELECT `date`, `number`, `distance`, `time` FROM radars WHERE `id` = ' . $_GET["id"];
+        $sql = 'SELECT * FROM radars WHERE `id` = ' . $_GET["id"];
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             $values = $result->fetch_assoc();
@@ -44,8 +44,8 @@ $values = [];
             $_POST['distance'], $_POST['time'], $_POST['id']);
         $stmt->execute();
 
-//header("Location: " . $_SERVER['PHP_SELF']); /* Redirect browser */
-        //exit();
+        header("Location: " . $_SERVER['PHP_SELF']); /* Redirect browser */
+        exit();
 
     } else if ($_POST['id'] === '') {
         $insert = "INSERT INTO radars(`date`, `number`, `distance`, `time`) VALUES(?, ?, ?, ?)"; 
@@ -53,61 +53,68 @@ $values = [];
         $stmt->bind_param("ssdd", $_POST['date'], $_POST['number'], $_POST['distance'], $_POST['time']);
         $stmt->execute();
 
-        //header("Location: " . $_SERVER['PHP_SELF']); /* Redirect browser */
-        //exit();
+        header("Location: " . $_SERVER['PHP_SELF']); /* Redirect browser */
+        exit();
     }
 
 
 ?>
-<form method="post" action=""><br>
-<br><label>Greičio fiksavimo data ir laikas: </label><input name="date" type="text" value = "<?=$values['date'] ?>" </br></br>
-<br><label>Automobilio numeris, pvz: ABC 001: </label><input type="text" value = "<?=$values['number'] ?>"</br></br>
-<br><label>Nuvažiuotas atstumas metrais: </label><input type="text" value = "<?=$values['distance'] ?>"</br/></br>
-<br><label>Sugaištas laikas sekundėmis: </label><input type="text" value = "<?=$values['time'] ?>"</br/></br>
-<input name="id" type="hidden" value="<?= $values['id'] ?>">
-<br> <button type="submit">Įvesti</button></br>
-<br></form>
-<form action="" method="get"><button>Valyti</button></form></br>
+
+<form action="" method="post"><br>
+    <br><label>Greičio fiksavimo data ir laikas: </label>
+    <br><input name="date" type="text" value="<?= $values['date'] ?>"><br>
+    <br><label>Automobilio numeris, pvz: ABC 001: </label>
+    <br><input name="number" type="text" value="<?= $values['number'] ?>"><br>
+    <br><label>Nuvažiuotas atstumas metrais: </label>
+    <br><input name="distance" type="text" value="<?= $values['distance'] ?>"><br>
+    <br><label>Sugaištas laikas sekundėmis: </label>
+    <br><input name="time" type="text" value="<?= $values['time'] ?>">
+    
+    <input name="id" type="hidden" value="<?= $values['id'] ?>"><br>
+
+    <br><button>Įvesti</button><br>
+    
+</form>
+<br><form action="" method="get"><button>Valyti</button></form><br>
 
 <?php
 lentele($conn);
 
 
 function lentele($conn) {
-// išvedame
-$sql = 'SELECT `id`, `date`, `number`, `distance`, `time` FROM radars ORDER BY `id`, `date` DESC';
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    ?>
-    <table border=1px>
-        <tr>
-            <th>ID</th>
-            <th>Data ir laikas</th>
-            <th>Numeris</th>
-            <th>Atstumas, m</th>
-            <th>Laikas, s</th>
-            <th>Duomenų taisymas</th>
-            
-        </tr>
-    
-        <?php while($row = $result->fetch_assoc()): ?>
+    // išvedame
+    $sql = 'SELECT *, `distance`/`time`*3.6 as `speed` FROM radars ORDER BY `id` ASC';
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        ?>
+        <table border = 1px>
             <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td><?php echo $row['date']; ?></td>
-                <td><?php echo $row['number']; ?></td>
-                <td><?php echo $row['distance']; ?></td>
-                <td><?php echo $row['time']; ?></td>
-                <td><a href="?id=<?= $row['id'] ?>">Taisyti</a></td>
+                <th>ID</th>
+                <th>Automobilio numeris</th>
+                <th>Data</th>
+                <th>Atstumas, m</th>
+                <th>Laikas, s</th>
+                <th>Greitis km/h</th>
+                <th>Duomenų koregavimas</th>
             </tr>
-        <?php endwhile; ?>
-    
-    </table>
+        
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['number']; ?></td>
+                    <td><?php echo $row['date']; ?></td>
+                    <td><?php echo $row['distance']; ?></td>
+                    <td><?php echo $row['time']; ?></td>
+                    <td><?php echo round($row['speed']); ?></td>
+                    <td><a href="?id=<?= $row['id'] ?>">Taisyti</a></td>
+                </tr>
+            <?php endwhile; ?>
+        
+        </table>
 
-    <?php
-} else {
-    echo 'nėra duomenų';
+        <?php
+    } else {
+        echo 'nėra duomenų';
+    }
+    $conn->close();
 }
-$conn->close();
-}
-?>
-</body>
